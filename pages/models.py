706a6b1c3abde681901abb51dfc575b93251c7a8
catherine james <jamescartherine7@gmail.com>
2026-05-11 16:52:37 +0300

    teentalks2

--- pages/models.py
+++ pages/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Topic(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='topics/',blank=True,null=True)
+    image = models.ImageField(upload_to='topics/' ,blank=True,null=True)
 
     def __str__(self):
         return self.title
